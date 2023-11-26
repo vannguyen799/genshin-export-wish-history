25df0e07af5152c110562f021639ed0909e8f867
Van Nguyen | KT <nguyenbavan.dev@gmail.com>
2023-11-26 07:06:31 +0700

    commit

---
+++ test/test.py
@@ -0,0 +1,28 @@
+from src.data.genshin_local_file import GenshinLocalFile
+from src.model.user import User
+from src.crawler import *
+
+
+def export_xlsx_test(uid):
+    user = User(uid)
+    user.export_xlsx()
+
+
+def crawl_data_test(genshin_path='C:\Program Files\Genshin Impact\Genshin Impact game'):
+    GLF = GenshinLocalFile(genshin_path)
+    api = GLF.get_banner_history_api()
+    print(api)
+
+    CBC = CharacterBannerCrawler(api)
+    WBC = WeaponBannerCrawler(api)
+    NBC = NormalBannerCrawler(api)
+
+    uid = CBC.get_uid()
+    user = User(uid)
+    print(f'uid {uid}')
+    history_data = CBC.crawl(user.get_last_character_banner_id())
+    user.set_character_banner(history_data).save()
+    history_data = WBC.crawl(user.get_last_weapon_banner_id())
+    user.set_weapon_banner(history_data).save()
+    history_data = NBC.crawl(user.get_last_normal_banner_id())
+    user.set_normal_banner(history_data).save()
