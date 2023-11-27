238c9a9c39be8e419efc7a6c9701565f4597ed43
Van Nguyen | KT <nguyenbavan.dev@gmail.com>
2023-11-27 22:24:18 +0700

    commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.test.test import *
 from src.util import delete_exported_xlsx
 
+# 1. Open Genshin Impact Game then open banner history
+# 2. Run script
+
 if __name__ == '__main__':
     # Set your Genshin Impact game installation location
     # -- You can find it by open Genshin Launcher -> Setting -> Game resources
